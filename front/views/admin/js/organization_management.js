3b033bc034fde276ce3746fe5f0b3dba55989a9c
MennessierLucas <lucas.mennessier.pro@gmail.com>
2023-05-08 02:28:27 +0200

    orga management page -> Template de liste -> Template de navbar -> Template de section de recherche ->

--- front/views/admin/js/organization_management.js
+++ front/views/admin/js/organization_management.js
@@ -1,9 +1,10 @@
 $('.dropdown-el').click(function(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    $(this).toggleClass('expanded');
-    $('#'+$(e.target).attr('for')).prop('checked',true);
-  });
-  $(document).click(function() {
-    $('.dropdown-el').removeClass('expanded');
-  });
+  e.preventDefault();
+  e.stopPropagation();
+  $(this).toggleClass('expanded');
+  $('#'+$(e.target).attr('for')).prop('checked',true);
+});
+
+$(document).click(function() {
+  $('.dropdown-el').removeClass('expanded');
+});
