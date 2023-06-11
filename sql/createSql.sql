e679eabb59535eee70327f9f56601f3ad9783215
Lialix <li.alix.pro@gmail.com>
2023-06-11 19:23:27 +0200

    better sql creation script

--- sql/createSql.sql
+++ sql/createSql.sql
@@ -1,3 +1,12 @@
+-- Drop tables if they exist
+DROP TABLE IF EXISTS Pieces;
+DROP TABLE IF EXISTS Candidatures;
+DROP TABLE IF EXISTS Utilisateurs;
+DROP TABLE IF EXISTS Offres;
+DROP TABLE IF EXISTS Demandes_Creation_Organisation;
+DROP TABLE IF EXISTS Organisations;
+
+-- Create Organisations table
 CREATE TABLE Organisations (
     siren INT PRIMARY KEY,
     nom VARCHAR(255) NOT NULL,
@@ -9,8 +18,9 @@ CREATE TABLE Organisations (
     siege_social VARCHAR(255)
 );
 
-CREATE TABLE demandes_creation_organisation (
-    request_id INT AUTO_INCREMENT,
+-- Create Demandes_Creation_Organisation table
+CREATE TABLE Demandes_Creation_Organisation (
+    request_id INT PRIMARY KEY AUTO_INCREMENT,
     requester_id VARCHAR(100) NOT NULL,
     status ENUM('pending', 'accepted', 'rejected') NOT NULL,
     date DATE NOT NULL,
@@ -20,10 +30,10 @@ CREATE TABLE demandes_creation_organisation (
     nom VARCHAR(255) NOT NULL,
     domaine VARCHAR(255) NOT NULL,
     siege_social VARCHAR(255),
-	PRIMARY KEY (request_id, siren),
     FOREIGN KEY (requester_id) REFERENCES Utilisateurs(email)
 );
 
+-- Create Offres table
 CREATE TABLE Offres (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nom VARCHAR(255) NOT NULL,
@@ -33,13 +43,14 @@ CREATE TABLE Offres (
     rythme VARCHAR(255),
     salaire VARCHAR(255),
     description TEXT,
-    status ENUM('pending','hidden', 'accepted') NOT NULL,
+    status ENUM('pending', 'hidden', 'accepted') NOT NULL,
     date DATE NOT NULL,
     liste_piece VARCHAR(255) NOT NULL,
     siren INT NOT NULL,
-    FOREIGN KEY (siren) REFERENCES Organisation(siren)
+    FOREIGN KEY (siren) REFERENCES Organisations(siren)
 );
 
+-- Create Utilisateurs table
 CREATE TABLE Utilisateurs (
     email VARCHAR(100) PRIMARY KEY,
     nom VARCHAR(255) NOT NULL,
@@ -49,9 +60,10 @@ CREATE TABLE Utilisateurs (
     dateCreation DATE NOT NULL,
     role ENUM('Recruteur', 'Candidat', 'Administrateur') NOT NULL,
     id_orga INT NOT NULL,
-    FOREIGN KEY (id_orga) REFERENCES Organisation(siren)
+    FOREIGN KEY (id_orga) REFERENCES Organisations(siren)
 );
 
+-- Create Candidatures table
 CREATE TABLE Candidatures (
     id INT PRIMARY KEY AUTO_INCREMENT,
     status ENUM('pending', 'accepted', 'rejected') NOT NULL,
@@ -60,14 +72,15 @@ CREATE TABLE Candidatures (
     id_user VARCHAR(100) NOT NULL,
     id_offre INT NOT NULL,
     FOREIGN KEY (id_user) REFERENCES Utilisateurs(email),
-    FOREIGN KEY (id_offre) REFERENCES Offre(id),
-    FOREIGN KEY (siren) REFERENCES Organisation(siren),
+    FOREIGN KEY (id_offre) REFERENCES Offres(id),
+    FOREIGN KEY (siren) REFERENCES Organisations(siren),
     UNIQUE (id_user, id_offre)
 );
 
+-- Create Pieces table
 CREATE TABLE Pieces (
     id INT PRIMARY KEY AUTO_INCREMENT,
     file LONGBLOB NOT NULL,
     id_candidature INT NOT NULL,
-    FOREIGN KEY (id_candidature) REFERENCES Candidature(id)
+    FOREIGN KEY (id_candidature) REFERENCES Candidatures(id)
 );
