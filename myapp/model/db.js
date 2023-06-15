d72fa6b1072852797686196a07741e7d81f1d126
Li Alix <li.alix.lf@gmail.com>
2023-06-15 15:09:53 +0200

    node moduels

--- myapp/model/db.js
+++ myapp/model/db.js
@@ -2,7 +2,7 @@ var mysql = require("mysql");
 var pool = mysql.createPool({
 host: "localhost", //ou localhost
 user: "root",
-password: "",
+password: "Li0134102754*",
 database: "ai16"
 });
 module.exports = pool;  
